905ac44add08f959e0ce995268137c7a891b191d
eraser347 <eraser347@gmail.com>
2023-04-19 17:03:00 -0400

    Delete code directory

--- code/exp.py
+++
@@ -1,7 +0,0 @@
-from evaluation import eval_UCB, eval_HyRan, eval_SuplinUCB, eval_TS
-for d in [5, 10, 20]:
-    for N in [10, 20]:
-        #eval_UCB(N=N, d=d)
-        #eval_TS(N=N, d=d)
-        #eval_HyRan(N=N, d=d)
-        eval_SuplinUCB(N=N, d=d)
